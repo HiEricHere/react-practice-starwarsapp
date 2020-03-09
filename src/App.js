9e6965cc0d537d7ecde0272fec3cfbd75baae3bb
Eric Huang <ejhuang84@gmail.com>
2020-03-10 02:12:28 +0800

    base files

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Test from './components/Test';
+
+import './App.css';
+
+const App = () => {
+  return (
+    <div id="app">
+      <Test/>
+    </div>
+  );
+}
+
+export default App;
